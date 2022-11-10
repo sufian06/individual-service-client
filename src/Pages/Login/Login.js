6c8e63020fbf1bd01d043ad9409ed35cfa1489d2
sufian <sufian4se@gmail.com>
2022-11-10 21:40:31 +0600

    send data to database from client side

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -107,10 +107,10 @@ const Login = () => {
         <div className="mt-7">
           <button onClick={handleGoogleSignIn}
             type="button"
-            class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+            className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
           >
             <svg
-              class="mr-2 -ml-1 w-4 h-4"
+              className="mr-2 -ml-1 w-4 h-4"
               aria-hidden="true"
               focusable="false"
               data-prefix="fab"
--- src/Pages/Services/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider/AuthProvider";
 
 const ServiceDetails = () => {
   const service = useLoaderData();
-  console.log(service)
+  const { user } = useContext(AuthContext);
+  console.log(user);
+
+  const [comment, setComment] = useState();
+
+  const { name, img, price, details } = service;
+
+  const handleReview = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const comment = form.comment.value;
+    const email = user?.email || "unregistered";
+    const name = user?.displayName || "no name";
+
+    const review = {
+      comment,
+      email,
+      name,
+    };
+
+    fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(review),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          alert("review placed successfully");
+          form.reset();
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <div>
-      ServiceDetails
+    <div className="container mx-auto my-8">
+      <div className="max-w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img
+            className="rounded-t-lg w-full h-[500px] object-cover"
+            src={img}
+            alt=""
+          />
+        </a>
+        <div className="p-5">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {name}
+          </h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {details}
+          </p>
+        </div>
+      </div>
+
+      {/* all comments */}
+      <p>{comment}</p>
+
+      {/* reviews */}
+      <form onSubmit={handleReview}>
+        <label className="block" htmlFor="">
+          add your comment
+        </label>
+        <textarea className="block w-2/3" name="comment"></textarea>
+        <button
+          type="submit"
+          className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-2"
+        >
+          Add Comment
+        </button>
+      </form>
     </div>
   );
 };
